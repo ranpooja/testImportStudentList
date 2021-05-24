6f94962105b7120af0733bb70c2405ec73fcc56a
ranpooja <poojakarwal2002@gmail.com>
2021-05-24 21:21:48 +0530

    branch file

---
+++ src/main/java/studentList/partTimeStudent.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentList;
+
+/**
+ *this is for git demo only
+ * @author pooja
+ */
+public class partTimeStudent {
+    
+}
